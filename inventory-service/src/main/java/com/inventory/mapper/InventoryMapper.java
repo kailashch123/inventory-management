1e09f4621fde7b4d5e6d0a1b657cdec08b33e372
kailash <kailash@example.com>
2026-09-09 01:50:01 +0530

    correction in inventory service

--- inventory-service/src/main/java/com/inventory/mapper/InventoryMapper.java
+++ inventory-service/src/main/java/com/inventory/mapper/InventoryMapper.java
@@ -12,12 +12,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface InventoryMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "reservedQuantity", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Inventory toEntity(InventoryRequest request);
 
-    @Mapping(
-            target = "availableQuantity",
-            expression = "java(getAvailableQuantity())"
-    )
     InventoryResponse toResponse(Inventory inventory);
 
     List<InventoryResponse> toResponseList(List<Inventory> productList);
